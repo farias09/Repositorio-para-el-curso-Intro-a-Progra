3cd937e4270e5f0c62550070faa788689adc4188
farias09 <f.ariasartavia09@gmail.com>
2021-10-21 10:51:34 -0700

    Primera Sincronización

---
+++ Documentos/Proyecto/src/libraries/Carro.java
@@ -0,0 +1,16 @@
+package libraries;
+
+public class Carro {
+
+    String NombreCarro;
+    String TipoCarro;
+    int CantidadPersonas;
+    int NumeroPlaca;
+    int PrecioRenta;
+    public Carro(String NombreCarro,String TipoCarro, int NumeroPlaca, int PrecioRenta){
+        this.NombreCarro = NombreCarro;
+        this.TipoCarro = TipoCarro;
+        this.NumeroPlaca = NumeroPlaca;
+        this.PrecioRenta = PrecioRenta;
+    }
+}
---
+++ Documentos/Proyecto/src/libraries/Cliente.java
@@ -0,0 +1,16 @@
+package libraries;
+
+public class Cliente {
+    String NombreDeCliente;
+    String Email;
+    int EdadCliente;
+    int Cedula;
+    int Telefono;
+
+    public Carro(String NombreDeCliente,String Email, int EdadCliente, int Cedula, int Telefono){
+        this.NombreDeCliente = NombreDeCliente;
+        this.Email = Email;
+        this.EdadCliente = EdadCliente;
+        this.Cedula = Cedula;
+        this.Telefono = Telefono;
+}
